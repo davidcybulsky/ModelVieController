5cfc4448b36050c63417c9688e57ba319991dc93
Dawid Cybulski <dawid.cybulsky@interia.pl>
2023-11-11 11:42:52 +0100

    Initial commit

---
+++ Models/MVCContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ModelVieController.Models
+{
+    public class MVCContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public MVCContext(DbContextOptions<MVCContext> options) : base(options) { }
+    }
+}
